aa85afae954f8e049235cde3f727ff51de573f88
connorwstein <connor.stein@mail.mcgill.ca>
2016-02-20 22:51:44 -0500

    can create a team of players from cbssports.com

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager, UserMixin, login_user, logout_user,\
-    current_user
+from flask.ext.login import LoginManager, UserMixin, login_user, logout_user, current_user
 from oauth import OAuthSignIn
-
+from golfparser import get_player_data
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret!'
@@ -26,29 +25,70 @@ class User(UserMixin, db.Model):
     social_id = db.Column(db.String(64), nullable=False, unique=True)
     nickname = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), nullable=True)
-    players = db.relationship('Player', backref='user',
-                                lazy='dynamic')
-
+    players = db.relationship('Player', backref='user', lazy='dynamic')
 
 class Player(db.Model):
     __tablename__ = 'players'
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(64), nullable=False, unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(64), nullable=False, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
 
-@app.route('/add_player', methods=['GET', 'POST'])
-def add_player():
-    print(request.form['player'])
-    print("Adding player")
-    return render_template('index.html')
 
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+@app.route('/home')
+def home():
+    if not current_user.is_authenticated:
+        return lm.unauthorized()
+    # get all the players currently on the users team
+    user = load_user(current_user.get_id())
+    players = Player.query.filter_by(user_id = user.id)
+    names = [player.name for player in players]  
+    print(names)
+    return render_template('home.html', active = "home", players = names)
+    
+def add_player_to_users_team(user, player_name):
+    players = Player.query.filter_by(name = player_name)
+    for player in players:
+        if(player.user_id == None):
+            #No one has claimed this player yet
+            player.user_id = user.id
+            db.session.commit()
+            flash("Successfully added player "+ player.name)
+        elif(player.user_id == user.id):
+            flash(player.name+" is already on your team!")
+        else:
+            #Player has be claimed, need to create a duplicate player
+            duplicate_player = Player()
+            duplicate_player.name = player
+            duplicate_player.user_id = user.id
+            db.session.add(duplicate_player)
+            db.session.commit()
+            flash("Successfully added player "+ player.name)
+
+
+@app.route('/add_players', methods=['GET', 'POST'])
+def add_players():
+    #player selection has occurred
+    if not current_user.is_authenticated:
+        return lm.unauthorized()
+    if request.method == 'POST':
+        print(load_user(current_user.get_id()).nickname)
+        user = load_user(current_user.get_id())
+        for selected_player in request.form:
+            #add each selected player to the users db
+            if(selected_player != "Submit"):
+                add_player_to_users_team(user, selected_player)
+    # need to list all the players in the database
+    names = [player.name for player in Player.query.all()]        
+    return render_template('add_players.html', active = "add_players", players = names)
 
 @app.route('/')
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     return render_template('index.html')
 
 
@@ -60,6 +100,7 @@ def logout():
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
+    print("authorize")
     if not current_user.is_anonymous:
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
@@ -68,6 +109,7 @@ def oauth_authorize(provider):
 
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
+    print("callback")
     if not current_user.is_anonymous:
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
@@ -78,16 +120,24 @@ def oauth_callback(provider):
     user = User.query.filter_by(social_id=social_id).first()
     if not user:
         user = User(social_id=social_id, nickname=username, email=email)
-        # player = Player()
-        # player.name = "Tiger"
-        # player.users = [user.id]
-        # user.players.append(player)
         db.session.add(user)
         db.session.commit()
     login_user(user, True)
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
 
+
+def add_all_players():
+    player_names = get_player_data()
+    print(player_names)
+    for i in range(len(player_names)):
+        player = Player()
+        player.name = player_names[i]
+        db.session.add(player)
+    db.session.commit()
 
 if __name__ == '__main__':
+    #clear_db()
+    db.drop_all()
     db.create_all() 
+    add_all_players()
     app.run(debug=True)
---
+++ golfparser.py
@@ -0,0 +1,65 @@
+__author__ = 'Bernie'
+import urllib2
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+def get_player_data():
+    driver = webdriver.PhantomJS()
+    driver.get("http://www.cbssports.com/golf/leaderboard/pga-tour/1186070/hyundai-tournament-of-champions")
+
+    playerDict={}
+    html = driver.page_source
+    parsed_html = BeautifulSoup(html,'html.parser')
+    tournaments = parsed_html.find("tbody", {"id": "LeaderboardSchedule"})
+    leaderboard =  parsed_html.find("tbody", {"id": "LeaderboardData"})
+
+    listEarn=[]
+    row1 = leaderboard.findAll("tr",{"class","row1"})
+    row2 = leaderboard.findAll("tr",{"class","row2"})
+
+    for row in row1:
+        items=row.findAll("td")
+        name=items[3]
+        rank =items[1]
+        earn =items[5]
+        #print(name.find("a").getText()+","+rank.find("dd").getText()+","+earn.find("dt").getText())
+        tup=(rank.find("dd").getText(),earn.find("dt").getText())
+        playerDict[name.find("a").getText().split(" PC",1)[0]]=tup
+
+    for row in row2:
+        items=row.findAll("td")
+        name=items[3]
+        rank =items[1]
+        earn =items[5]
+        #print(name.find("a").getText()+","+rank.find("dd").getText()+","+earn.find("dt").getText())
+        tup=(rank.find("dd").getText(),earn.find("dt").getText())
+        playerDict[name.find("a").getText().split(" PC",1)[0]]=tup
+
+    return playerDict.keys()
+
+
+#print(playerDict)
+#
+#
+#
+#
+#
+#
+#for earning in list:
+#    earn =earning.find("dt")
+#    if earn != None:
+#        #print(earn.getText())
+#        listEarn.append(earn.getText())
+#
+#list = parsed_html.findAll("td",{"class","playername"})
+#for player in list:
+#    name =player.find("a")
+#    if name != None:
+#        if(listEarn.is)
+#        playerDict[name.getText()]=listEarn.pop(0)
+#        #print(name.getText())
+#print playerDict
+#
+#
+##print parsed_html.body.find('div', attrs={'class':'container'}).text
